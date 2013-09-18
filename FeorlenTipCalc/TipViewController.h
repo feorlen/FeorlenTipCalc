0795ea17b5da808cb43e457564890d7360473051
feorlen <feorlen@Feorlens-MacBook-Pro.local>
2013-09-17 21:24:42 -0700

    View built

---
+++ FeorlenTipCalc/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  FeorlenTipCalc
+//
+//  Created by Feorlen on 9/17/13.
+//  Copyright (c) 2013 Feorlen.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ FeorlenTipCalc/TipViewController.m
@@ -0,0 +1,39 @@
+//
+//  TipViewController.m
+//  FeorlenTipCalc
+//
+//  Created by Feorlen on 9/17/13.
+//  Copyright (c) 2013 Feorlen.org. All rights reserved.
+//
+
+#import "TipViewController.h"
+
+@interface TipViewController ()
+
+@end
+
+@implementation TipViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        self.title = @"FeorlenTipCalc";
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
